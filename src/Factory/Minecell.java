caf0ce92656a80ce5b5753b29dd164f27206a7e1
Tinkerinio <alsalim.yernazarov@gmail.com>
2024-11-06 00:54:44 +0500

    Create types of cells

---
+++ src/Factory/Minecell.java
@@ -0,0 +1,31 @@
+﻿// Клетка с миной
+public class MineCell extends Cell {
+    public MineCell() {
+        this.isMine = true;
+    }
+
+    @Override
+    public String display() {
+        return isRevealed ? "*" : (isFlagged ? "F" : "");
+    }
+}
+
+// Клетка с числом
+public class NumberCell extends Cell {
+    public NumberCell(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
+
+    @Override
+    public String display() {
+        return isRevealed ? String.valueOf(adjacentMines) : (isFlagged ? "F" : "");
+    }
+}
+
+// Пустая клетка
+public class EmptyCell extends Cell {
+    @Override
+    public String display() {
+        return isRevealed ? " " : (isFlagged ? "F" : "");
+    }
+}
