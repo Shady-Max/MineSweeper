ccfd0cf6467bec1ab579ee95b015b050c6003270
Tinkerinio <alsalim.yernazarov@gmail.com>
2024-11-06 01:34:25 +0500

    Creating Factory method
    I implement in Factory method types of cells like empty, mine, and number cells. Cell Factory creates this cells on board

---
+++ src/Factory/EmptyCell.java
@@ -0,0 +1,10 @@
+﻿package Factory;
+
+import MVC.Cell;
+
+public class EmptyCell extends Cell {
+    @Override
+    public String display () {
+        return isRevealed() ? " " : (isFlagged() ? "F" : "");
+    }
+}
